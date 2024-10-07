ca12e8de42a7959eee35a069795182710f9e67f0
ashwinnagelwar <ashwinnagelwar@gmail.com>
2024-10-07 16:17:41 +0530

    first commit| All services created

---
+++ Online-Mobile-Buy-Service/src/main/java/com/onlineMobileBuyService/OnlineMobileBuyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.onlineMobileBuyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMobileBuyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMobileBuyServiceApplication.class, args);
+	}
+
+}
